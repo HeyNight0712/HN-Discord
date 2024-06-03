a04c5afb5fdb2faef2a446f6e8f4d905711a5d44
blacknight0712 <blacknight0981@gmail.com>
2024-06-03 15:23:14 +0800

    test Plugin.java

--- src/main/java/hn/blacknight0712/plugins/Plugin.java
+++ src/main/java/hn/blacknight0712/plugins/Plugin.java
@@ -1,13 +1,13 @@
 package hn.blacknight0712.plugins;
 
+import hn.blacknight0712.utils.LoggerManager;
 import org.slf4j.Logger;
 
-public interface Plugin {
-    void onEnable();
-
-    void onDisable();
-
-    String getName();
-
-    Logger getLogger();
+public abstract class Plugin {
+    public abstract void onEnable();
+    public abstract void onDisable();
+    public abstract String getName();
+    public Logger getLogger() {
+        return LoggerManager.getLogger(getName());
+    }
 }
--- src/main/java/hn/blacknight0712/plugins/PluginManager.java
+++ src/main/java/hn/blacknight0712/plugins/PluginManager.java
@@ -5,12 +5,14 @@ import hn.blacknight0712.utils.LoggerManager;
 import org.slf4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.jar.JarFile;
 
 public class PluginManager {
     private List<Plugin> plugins = new ArrayList<>();
@@ -36,10 +38,13 @@ public class PluginManager {
             for (File jarFile: jarFiles) {
                 try {
                     URL jarURL = jarFile.toURI().toURL();
+                    LoggerManager.info("測試路徑 " + jarURL);
                     URLClassLoader classLoader = new URLClassLoader(new URL[]{jarURL}, this.getClass().getClassLoader());
                     ServiceLoader<Plugin> serviceLoader = ServiceLoader.load(Plugin.class, classLoader);
                     for (Plugin plugin: serviceLoader) {
                         plugins.add(plugin);
+                        plugin.onEnable();
+                        LoggerManager.info("加載測試 " + plugin.getName());
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
